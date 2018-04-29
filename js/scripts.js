dfe7a2d073a04c08892b8ce6ded0d3f3108bc079
Dorota Kazak <dorota.kazak@gmail.com>
2018-04-29 11:51:12 +0200

    Delete git repository from main folder Kodilla-js

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+/*var a=12,
+	h=13;
+
+var triangleArea=a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+/* Zadeklaruj zmienne a, h oraz triangleArea. 
+Nadaj im wartość początkową dla a oraz h (jakieś liczby).
+Przypisz do triangleArea działanie a*h/2.
+Na koniec wypisz
+
+ console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+Do pobrania danych możesz także skorzystać z funkcji prompt*/
+
+var a = prompt('Wstaw liczbe podstawy trojkata');
+var h = prompt('Wstaw liczbe wysokosci trojkata');
+var triangleArea=a*h/2;
+alert('Gratulacje! Pole twojego trojkata to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+/*console.log('Hello, ' + name);
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);*/
